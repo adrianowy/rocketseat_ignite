e89f30741e2b962b0d06f6093ab15d3f7bfa09b7
adrianowy <adrianoabreu.wy@gmail.com>
2021-06-06 23:03:02 -0400

    chapterII

---
+++ chapter_2/dtmoney/src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import logoImg from "../../assets/logo.svg"
+
+
+
+export function Header(){
+    return (
+        <header>
+            <img src={logoImg} alt="dt money" />
+            <button type="button">
+                Nova Transação
+            </button>
+        </header>
+    )
+}
