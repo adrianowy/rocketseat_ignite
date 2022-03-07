297206250d8f4422b5325367c6a6db68c36f2767
Adriano de Abreu Wanderley <adrianoabreu.wy@gmail.com>
2022-03-07 16:50:33 +0000

    auth

--- 02-ignews/src/pages/index.tsx
+++ 02-ignews/src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { GetServerSideProps } from 'next';
+// import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
 import { stripe } from '../services/stripe';
@@ -35,13 +36,37 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
+/**
+ * getStaticProps é usado para página estáticas SSR (Static Site Generation)
+ * só posso usar dessa maneira em páginas que são iguais para todos que vão acessar a aplicação
+ * e também são importantes para indexação no google SEO
+*/
+
+export const getStaticProps: GetStaticProps = async () => {
+
+  const price = await stripe.prices.retrieve('price_1KZhUzHNjqmhZuZiOII25kIC')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+    // amount: (price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product
+    },
+    // quanto tempo em segundos essa página vai se manter sem ser revalidada (reconstruida)
+    revalidate: 60 * 60 * 24, // 24 hours
+  }
+};
+
+/**
 export const getServerSideProps: GetServerSideProps = async () => {
 
-  /**
-   * const price = await stripe.prices.retrieve('price_1KZhUzHNjqmhZuZiOII25kIC', {
-    expand:['product']
-  })
-   */
   const price = await stripe.prices.retrieve('price_1KZhUzHNjqmhZuZiOII25kIC')
 
   const product = {
@@ -59,5 +84,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     }
   }
 };
+*/
 
 // https://dashboard.stripe.com/settings/branding
