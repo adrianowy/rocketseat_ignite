cfafe7bace0e5a787792fc3b977638d3d211336a
Adriano Wanderley <adriano.wanderley@dmz.org.br>
2022-03-15 13:54:10 -0400

    conectando no prismic e mostrando o post completo

--- 02-ignews/src/pages/posts/index.tsx
+++ 02-ignews/src/pages/posts/index.tsx
@@ -1,9 +1,11 @@
 import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import { getPrismicClient } from '../../services/prismic'
-import styles from './styles.module.scss'
 import Prismic from '@prismicio/client'
 import { RichText } from 'prismic-dom'
+import Link from 'next/link'
+
+import styles from './styles.module.scss'
 
 type Post = {
     slug: string;
@@ -27,11 +29,13 @@ export default function Posts({ posts }: PostsProps){
                 <div className={styles.posts}>
                     {
                         posts.map(post => (
-                            <a href="#" key={post.slug}>
-                                <time>{post.updatedAt}</time>
-                                <strong>{post.title}</strong>
-                                <p>{post.excerpt}</p>
-                            </a>
+                            <Link href={`/posts/${post.slug}`} >
+                                <a key={post.slug}>
+                                    <time>{post.updatedAt}</time>
+                                    <strong>{post.title}</strong>
+                                    <p>{post.excerpt}</p>
+                                </a>
+                            </Link>
                         ))
                     }
                     
