39f80df4f5277dc4ab1e970cde5e874acecc559b
Adriano Wanderley <adriano.wanderley@dmz.org.br>
2022-04-04 22:01:40 -0400

    criando validacao de formulario

--- 04-admin-panel/src/components/Form/Input.tsx
+++ 04-admin-panel/src/components/Form/Input.tsx
@@ -1,13 +1,21 @@
-import {Input as ChakraInput, FormLabel, FormControl, InputProps as ChakraInputProps} from '@chakra-ui/react'
+import {
+  Input as ChakraInput, 
+  FormLabel, 
+  FormControl, 
+  InputProps as ChakraInputProps,
+  FormErrorMessage
+} from '@chakra-ui/react'
+import { FieldError } from 'react-hook-form'
 
 interface InputProps extends ChakraInputProps {
     name: string;
     label?: string;
+    error?: FieldError;
 }
 
-export function Input({name, label, ...rest}: InputProps) {
+export function Input({name, label, error, ...rest}: InputProps) {
     return (
-        <FormControl>
+        <FormControl isInvalid={!!error}>
             {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
             
             <ChakraInput
@@ -22,6 +30,15 @@ export function Input({name, label, ...rest}: InputProps) {
               size={'lg'}
               {...rest}
             />
+
+            {
+              !!error && (
+                <FormErrorMessage>
+                  {error?.message}
+                </FormErrorMessage>
+              )
+            }
+
         </FormControl>
     )
 }
--- 04-admin-panel/src/pages/index.tsx
+++ 04-admin-panel/src/pages/index.tsx
@@ -1,14 +1,24 @@
 import {Button, Flex, Stack} from '@chakra-ui/react'
-import { useForm } from 'react-hook-form'
+import { SubmitErrorHandler, useForm } from 'react-hook-form'
 import { Input } from '../components/Form/Input'
+
+type SignInFormData = {
+  email: string;
+  password: string;
+}
 
 
 export default function SignIn() {
 
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, formState } = useForm();
 
-  function handleSignIn(){
-    
+  const { errors } = formState;
+
+  const handleSignIn: SubmitErrorHandler<SignInFormData> = async (values) => {
+
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    console.log(values);
   }
 
   return (
@@ -25,15 +35,33 @@ export default function SignIn() {
         backgroundColor='gray.800'
         p={8}
         borderRadius={8}
-        flexDir={'column'}
+        flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
 
         <Stack spacing={4}>
-          <Input name='email' label='E-mail' type={'email'} />
-          <Input name='password' label='Senha' type={'password'} />
+          <Input
+            name="email"
+            label="E-mail"
+            type="email"
+            error={errors.email}
+            {...register('email', {required: 'E-mail is obrigatorio'})}
+            />
+          <Input
+            name="password"
+            label="Senha"
+            type="password"
+            error={errors.password}
+            {...register('password')}
+          />
         </Stack>
 
-        <Button type='submit' mt="6" colorScheme={'pink'}>
+        <Button
+          type='submit'
+          mt="6"
+          colorScheme={'pink'}
+          isLoading={formState.isSubmitting}
+        >
           Entrar
         </Button>
 
